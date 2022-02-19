72fc442b85c1c5d143685413e4f67fc98214b078
zakari64 <74094535+zakari64@users.noreply.github.com>
2022-02-19 16:49:49 +0100

    mid life cycle

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 
-import React from 'react';
-import Topnav from './components/Topnav';
-import Navb from './components/Navb';
-
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Oursite from './components/Oursite';
+import Products from './components/Products';
+import Owlprod from './components/Owlprod';
+import Chose from './components/Chose';
+import Experience from './components/Experience';
 function App() {
   return (
     <div className="App">
-     <Topnav />
-     <Navb />
+      <Navbar />
+      <Home />
+      <Oursite />
+      <Products />
+      <Owlprod />
+      <Chose />
+      <Experience />
     </div>
   );
 }
